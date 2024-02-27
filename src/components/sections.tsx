ac7f63d6e34a237cd8d43397eb50131b3ec865ea
João Bertotto <joaobertottoneto@gmail.com>
2024-02-26 23:08:12 -0300

    Add: Content and Section component

--- src/components/sections.tsx
+++ src/components/sections.tsx
@@ -1,18 +1,20 @@
 import type { Page } from '@/payload/payload-types'
-import { FeaturedGrid } from './featured-grid'
+import { FeaturedGrid } from '../app/_sections/featured-grid'
 import { ProductSlider } from './product-slider'
-import { Statistics } from './statistics'
+import { Statistics } from '../app/_sections/statistics'
+import { ContentSection } from '../app/_sections/content'
 
 const sectionComponents = {
   'product-carousel': ProductSlider,
   'featured-section': FeaturedGrid,
-  // 'statistic-section': Statistics,
+  'statistic-section': Statistics,
+  'content-section': ContentSection,
 }
 
 export function Sections({ sections }: { sections: Page['layout'][0][] }) {
   const sectionsExist =
     sections && Array.isArray(sections) && sections.length > 0
-
+  console.log(sections)
   if (sectionsExist) {
     return (
       <>
@@ -22,7 +24,10 @@ export function Sections({ sections }: { sections: Page['layout'][0][] }) {
             const Block = sectionComponents[blockType]
 
             if (Block) {
-              return <Block id={blockName} key={index} {...section} />
+              // TODO: fix this
+              // @ts-expect-error
+
+              return <Block key={index} {...section} />
             }
           }
         })}
