7e32895484b8c306031b06c43cdfdd72f79be6c6
João Bertotto <joaobertottoneto@gmail.com>
2024-03-15 18:01:30 -0300

    Changes: Image component

--- src/components/media/index.tsx
+++ src/components/media/index.tsx
@@ -1,11 +1,26 @@
-import React, { ElementType, Fragment } from 'react'
+import React, { ElementType, Fragment, Ref } from 'react'
+import type { StaticImageData } from 'next/image'
+
+import type { Media as MediaType } from '../../payload/payload-types'
 
 import { Image } from './image'
-import { Props } from './types'
 
-export const Media: React.FC<Props> = (props) => {
-  const { className, htmlElement = 'div' } = props
+export interface Props {
+  src?: StaticImageData // for static media
+  alt?: string
+  resource?: string | MediaType // for Payload media
+  size?: string // for next/image only
+  priority?: boolean // for next/image only
+  fill?: boolean // for next/image only
+  className?: string
+  imgClassName?: string
+  htmlElement?: ElementType | null
+  onClick?: () => void
+  onLoad?: () => void
+  ref?: Ref<null | HTMLImageElement>
+}
 
+export function Media({ className, htmlElement = 'div', ...props }: Props) {
   const Tag = (htmlElement as ElementType) || Fragment
 
   return (
