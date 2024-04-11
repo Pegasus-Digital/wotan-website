9b2f5be3caef070627a103e25d271bb579d12b01
João Bertotto <joaobertottoneto@gmail.com>
2024-04-10 22:57:35 -0300

    fix: clients grid

--- src/app/_sections/client-grid.tsx
+++ src/app/_sections/client-grid.tsx
@@ -1,4 +1,4 @@
-import { Media } from '@/components/media'
+import { Image } from '@/components/media/image'
 import { H1 } from '@/components/typography/headings'
 import { Lead } from '@/components/typography/texts'
 import { Page, ClientGrid as ClientGridType } from '@/payload/payload-types'
@@ -20,13 +20,18 @@ export default function ClientGrid({
           <Heading variant='h2'>{title}</Heading>
           <Lead>{description}</Lead>
         </div>
-        <div className='grid max-w-screen-desktop grid-cols-2 gap-4 tablet:grid-cols-4 tablet:gap-8 desktop:grid-cols-5'>
+        <div className='grid max-w-screen-desktop grid-cols-2 gap-4 tablet:grid-cols-4 tablet:gap-6 desktop:grid-cols-5'>
           {clients.map((client, index) => (
-            <Media
+            <div
               key={index}
-              resource={client.logo}
-              className='m-2 aspect-square h-full max-h-48 max-w-48  rounded-md bg-white object-cover p-2 shadow-wotan-light'
-            />
+              className='m-2  flex aspect-square h-full  max-h-48  max-w-48 items-center justify-center overflow-hidden rounded-md bg-white object-cover p-2 shadow-wotan-light'
+            >
+              <Image
+                key={index}
+                resource={client.logo}
+                // className='m-2  aspect-square h-full max-h-48  max-w-48  rounded-md bg-white object-cover p-2 shadow-wotan-light'
+              />
+            </div>
           ))}
         </div>
       </div>
