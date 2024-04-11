a7e325717124ed877a97a1f8087538912c13b328
João Bertotto <joaobertottoneto@gmail.com>
2024-04-10 21:59:12 -0300

    fix: adding new search params and pagination

--- src/app/(website)/pesquisa/page.tsx
+++ src/app/(website)/pesquisa/page.tsx
@@ -2,32 +2,74 @@ import payload from 'payload'
 
 import { SearchPageContent } from './content'
 
+import { getSingular } from './../../../lib/singular'
+
 export default async function CategoryPage({ params, searchParams }) {
   const page: number = searchParams ? Number(searchParams.page) : 1
   const query: string = searchParams ? String(searchParams.query) : ''
 
+  const words = query.split(' ').filter((word) => word.length >= 3)
+
+  function createObjectsForWords(words) {
+    const objects = []
+
+    words.forEach((word, index) => {
+      if (index > 0) {
+        objects.push({
+          or: [
+            {
+              title: {
+                contains: getSingular(word),
+              },
+            },
+            {
+              description: {
+                contains: getSingular(word),
+              },
+            },
+          ],
+        })
+      }
+    })
+
+    return objects
+  }
+
+  const searchFields = [
+    'title',
+    'sku',
+    'categories.breadcrumbs.label',
+    'description',
+  ]
+
+  // Define a function to generate conditions for a single word
+  const generateWordConditions = (word) => {
+    return searchFields.map((field) => ({
+      [field]: {
+        contains: getSingular(word),
+      },
+    }))
+  }
+
+  const whereQuery = {
+    or: [
+      // Conditions for the first word
+      ...generateWordConditions(words[0]),
+    ],
+    and: createObjectsForWords(words),
+  }
+
+  console.log(whereQuery)
+
   const { docs, ...paginationParams } = await payload.find({
     collection: 'products',
-    where: {
-      or: [
-        {
-          title: {
-            contains: query,
-          },
-        },
-        {
-          sku: {
-            contains: query,
-          },
-        },
-      ],
-    },
+    where: whereQuery,
+
     limit: 20,
     page: page,
   })
 
   return (
-    //@ts-ignore
     <SearchPageContent
       products={docs}
       pagination={paginationParams}
