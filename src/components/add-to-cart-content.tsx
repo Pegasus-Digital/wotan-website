a4308772c39615bb472e0d6eb6931cf27a99a47c
João Bertotto <joaobertottoneto@gmail.com>
2024-08-08 11:58:02 -0300

    change: dialog size on smaller screens

--- src/components/add-to-cart-content.tsx
+++ src/components/add-to-cart-content.tsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 
 import { useEffect, useState } from 'react'
 
@@ -8,12 +8,14 @@ import { LoadingSpinner } from './spinner'
 import { ScrollArea, ScrollBar } from './ui/scroll-area'
 import { ProductGallery } from '@/app/(website)/produtos/[slug]/(components)/product-gallery'
 import { ProductInteraction } from '@/app/(website)/produtos/[slug]/(components)/product-interaction'
+import { Button } from '@/pegasus/button'
 
 interface AddToCartFormProps {
   productId: string
+  setOpen: (state: boolean) => void
 }
 
-export function AddToCartContent({ productId }: AddToCartFormProps) {
+export function AddToCartContent({ productId, setOpen }: AddToCartFormProps) {
   const [product, setProduct] = useState<Product>(null)
 
   // Refetch product, just to be sure
@@ -39,14 +41,24 @@ export function AddToCartContent({ productId }: AddToCartFormProps) {
   }, [])
 
   return product ? (
-    <ScrollArea className='max-h-[48rem] py-4'>
+    <ScrollArea className='max-h-[calc(100vh-10rem)] py-4 tablet:max-h-[32rem] desktop:max-h-[48rem]'>
       <div className=' grid w-full grid-cols-1 place-items-stretch gap-2 overflow-y-auto md:grid-cols-2'>
         <ProductGallery product={product} />
-        <ProductInteraction product={product} biggerQuantity={false} />
+        <div className='flex flex-col items-end justify-end gap-4'>
+          <ProductInteraction product={product} biggerQuantity={false} />
+          <Button
+            // className='w-full'
+            variant='outline'
+            size='lg'
+            onClick={() => setOpen(false)}
+          >
+            Voltar para a navegação
+          </Button>
+        </div>
       </div>
     </ScrollArea>
   ) : (
-    <div className='flex h-full w-full items-center justify-center'>
+    <div className='flex h-[calc(100vh-12rem)] w-full items-center justify-center '>
       <LoadingSpinner />
     </div>
   )
